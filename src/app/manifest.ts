e806a1267d31674f2ed793b92ad09745340af72e
Albin Frick <albinfrick@proton.me>
2025-04-22 15:12:22 +0200

    feat: use color-base for the theme and background on pwa (#134)

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,25 +1,25 @@
-import type { MetadataRoute } from 'next'
+import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
-  return {
-    name: 'Habbits',
-    short_name: 'Habbits',
-    description: 'A app for tracking all your habits',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#ffffff',
-    theme_color: '#000000',
-    icons: [
-      {
-        src: '/rabbit.svg',
-        sizes: '192x192',
-        type: 'image/svg',
-      },
-      {
-        src: '/rabbit.svg',
-        sizes: '512x512',
-        type: 'image/svg',
-      },
-    ],
-  }
+	return {
+		name: "Habbits",
+		short_name: "Habbits",
+		description: "A app for tracking all your habits",
+		start_url: "/",
+		display: "standalone",
+		background_color: "var(--color-base)",
+		theme_color: "var(--color-base)",
+		icons: [
+			{
+				src: "/rabbit.svg",
+				sizes: "192x192",
+				type: "image/svg",
+			},
+			{
+				src: "/rabbit.svg",
+				sizes: "512x512",
+				type: "image/svg",
+			},
+		],
+	};
 }
