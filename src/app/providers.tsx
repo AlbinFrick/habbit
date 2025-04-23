f29ce683257f0955b6379ca15f7531e2ca7a55ee
Albin Frick <abbzez@gmail.com>
2025-04-23 09:05:06 +0200

    feat: add page view tracking

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,17 +5,20 @@ import posthog from 'posthog-js'
 import { PostHogProvider as PHProvider } from 'posthog-js/react'
 import { useEffect } from 'react'
 import { env } from '@/env'
+import PostHogPageView from '@/components/PostHogPageView'
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
       api_host: env.NEXT_PUBLIC_POSTHOG_HOST,
-      capture_pageview: false // Disable automatic pageview capture, as we capture manually
+      capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+      capture_pageleave: true,
     })
   }, [])
 
   return (
     <PHProvider client={posthog}>
+      <PostHogPageView />
       {children}
     </PHProvider>
   )
